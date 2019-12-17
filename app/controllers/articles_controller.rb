5a545519b3177baa17cdc762f99140b9d9462daf
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2019-12-17 13:58:23 +0530

    [#5.3 #5.6] Created Show action and added flash meassges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article is created successfully"
+      redirect_to article_path(@article)
+    else
+      flash.now[:notice] = "Article is failed to be saved try again!"
+      render "new"
+    end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:name, :description)
+    end
 
 end
