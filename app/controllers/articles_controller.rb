26050e72913f7486b420a0baac4b4be16fefa98e
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2019-12-17 17:04:45 +0530

    Added destroy action and made articles_controller DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -8,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def create
@@ -23,11 +25,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update 
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article is successfully updated"
       redirect_to article_path(@article)
@@ -36,7 +37,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article is successfully destroyed"
+    redirect_to articles_path
+  end
+
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
